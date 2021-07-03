681b9b109611ffcef3f3ff7d30f6bf2320086084
olivier rozan <rozan.olivier@gmail.com>
2021-07-03 19:46:20 +0200

    [M-5] UI

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #3F4050;
+  height: 100vh;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,99 @@
 <template>
   <section id="home">
-    <h1>Mastermind</h1>
+    <h1 class="py-2">Mastermind</h1>
     <div id="score">
       <h2 v-if="nbCorrect === 4">GAGNE</h2>
       <h2 v-if="currentLine > 9">PERDU</h2>
     </div>
       
     <b-row id="home-wrapper">
-      <b-col id="board" cols="6">
-        <div id="line" :class="`top-${currentLine + 1}`"></div>
-        <b-table-simple id="board-table">
-          <b-tbody>
-            <b-tr class="board-row" :class="i" v-for="i in 10" :key="i">
-              <b-td
-                class="board-col"
-              >
-                {{ i }}
-              </b-td>
-              <b-td
-                class="board-col"
-                :class="`${i - 1}-${j - 1}`"
-                v-for="j in 4"
-                :key="j"
-              >
-              </b-td>
-              <b-td
-                class="board-col"
-              >
-                <!-- <span class="pion-1 mx-1"></span>
-                <span class="pion-2 mx-1"></span>
-                <span class="pion-3 mx-1"></span>
-                <span class="pion-4 mx-1"></span> -->
-              </b-td>
-            </b-tr>
-          </b-tbody>
-        </b-table-simple>
-      </b-col>
-      <b-col id="menu" cols="6">
-        <b-button-group vertical>
-          <b-button class="my-1" variant="info">Nouvelle partie</b-button>
-          <b-button class="my-1" variant="info">Vider la ligne</b-button>
-          <b-button
-            class="my-1"
-            variant="info"
-            @click="submitLine()"
-            :disabled="!isPlaying"
-            >Valider la ligne</b-button
+      <b-col id="board" class="m-auto" cols="4">
+        <b-card 
+          class="one-row"
+          v-for="(one, index) in grille"
+          :key="index"
+          no-body
+        >
+          <b-card 
+            class="one-column"
+            no-body
           >
-        </b-button-group>
-
-        <b-table-simple id="pattern-table" class="mt-3" v-if="isPlaying">
-          <b-tbody>
-            <b-tr
-              class="pattern-row"
-              :class="i"
-              v-for="(i, index) in colorsList"
-              :key="index"
+            <div
+              class="one-cell"
+              v-for="(one2, index2) in one"
+              :key="index2"
             >
-              <b-td
-                class="pattern-col"
-                :class="j"
-                v-for="j in 4"
-                :key="j"
-                @click="chooseColor(index, j)"
-              ></b-td>
-            </b-tr>
-          </b-tbody>
-        </b-table-simple>
+              <div
+                class="pion"
+                :class="{
+                          target: selectedRow === index && selectedColumn === index2,
+                          idle: selectedRow === index,
+                          red: grille[index][index2] === 0,
+                          blue: grille[index][index2] === 1,
+                          green: grille[index][index2] === 2,
+                          orange: grille[index][index2] === 3
+                        }"
+                @click="chooseCell(index, index2)"
+              ></div>
+            </div>
+
+            <div class="marker">
+              y
+            </div>
+          </b-card>
+        </b-card>
+
+        <b-card
+          id="pattern"
+          class="mt-5"
+          no-body
+        >
+          <div class="one-cell">
+            <div
+              class="pion red"
+              @click="chooseColor(0)"
+            ></div>
+          </div>
+          <div class="one-cell">
+            <div
+              class="pion blue"
+              @click="chooseColor(1)"
+            ></div>
+          </div>
+          <div class="one-cell">
+            <div
+              class="pion green"
+              @click="chooseColor(2)"
+            ></div>
+          </div>
+          <div class="one-cell">
+            <div
+              class="pion orange"
+              @click="chooseColor(3)"
+            ></div>
+          </div>
+          <div  class="one-cell">
+            <b-button
+              class="w-100"
+              variant="primary"
+              @click="submitLine()"
+              :disabled="selectedColumn < 4"
+            >
+              Valider
+            </b-button>
+          </div>
+        </b-card>
       </b-col>
     </b-row>
+
+    <b-modal id="modal-1" title="Règles du jeu">
+      <p class="my-4">Trouve la combinaison correcte.</p>
+      <div><span class="pion mx-1 black"></span> Un pion de couleur n'est pas dans la combinaison.</div>
+      <div><span class="pion mx-1 red"></span>Un pion de couleur est bien dans la combinaison mais pas à la bonne place.</div>
+      <div><span class="pion mx-1 green"></span>Un pion de couleur est bien dans la combinaison et à la bonne place</div>
+      <div></div>
+      <div></div>
+    </b-modal>
   </section>
 </template>
 
@@ -89,7 +114,10 @@ export default {
       soluce: [],
       nbWrongPlace: 0,
       nbIncorrect: 0,
-      nbCorrect: 0
+      nbCorrect: 0,
+      selectedRow: 0,
+      selectedColumn: 0,
+      selectedColor: ''
     };
   },
   created: function () {
@@ -105,6 +133,12 @@ export default {
         this.grille[this.currentLine][3] === -1;
     },
     init: function () {
+      /*
+        rouge: 0
+        bleu: 1
+        vert: 2
+        orange: 3
+      */
       this.grille = [
         [-1, -1, -1, -1],
         [-1, -1, -1, -1],
@@ -119,25 +153,34 @@ export default {
       ];
       this.soluce = [1, 2, 1, 2];
     },
-    chooseColor: function (i, j) {
-      this.selectedX = i;
-      this.selectedY = j;
-      const table = document.getElementById("board-table");
-      const tr = table
-        .getElementsByClassName("board-row")
-        .item(this.currentLine);
-      const td = tr.getElementsByClassName("board-col").item(this.selectedY);
-      td.style.backgroundColor = this.colorsList[this.selectedX];
-      this.grille[this.currentLine][this.selectedY] = this.selectedX;
-      // console.log(this.soluce, this.grille[this.currentLine]);
+    chooseCell: function (index, index2) {
+      if (this.selectedRow === index) {
+        this.selectedColumn = index2;
+      }
+    },
+    chooseColor: function (color) {
+      this.selectedColor = color;
+
+      if (this.selectedColumn < 4) {
+        this.grille[this.selectedRow][this.selectedColumn] = color;
+        this.selectedColumn++;
+      }
+      // this.selectedX = i;
+      // this.selectedY = j;
+      // const table = document.getElementById("board-table");
+      // const tr = table
+      //   .getElementsByClassName("board-row")
+      //   .item(this.currentLine);
+      // const td = tr.getElementsByClassName("board-col").item(this.selectedY);
+      // td.style.backgroundColor = this.colorsList[this.selectedX];
+      // this.grille[this.currentLine][this.selectedY] = this.selectedX;
     },
     submitLine: function () {
-      // console.log('soluce', this.soluce);
-      // console.log('proposition', this.grille[this.currentLine]);
-      this.checkSoluce();
-      this.currentLine++;
+      // this.checkSoluce();
+      this.selectedRow++;
+      this.selectedColumn = 0;
 
-      if (this.currentLine > 9) {
+      if (this.selectedRow > 9) {
         this.isPlaying = false;
       }
     },
@@ -184,6 +227,45 @@ export default {
 </script>
 
 <style lang="scss">
+.pion {
+  width: 25px;
+  height: 25px;
+  display: inline-block;
+  border-radius: 100%;
+}
+
+.idle {
+  background: grey;
+}
+
+.target {
+  background: white;
+}
+
+.red {
+  background: red;
+}
+
+.green {
+  background: green;
+}
+
+.blue {
+  background: blue;
+}
+
+.orange {
+  background: orange;
+}
+
+.card {
+  background: transparent !important;
+  border-color: white !important;
+  border-left: 0 !important;
+  border-right: 0 !important;
+  border-radius: 0 !important;
+}
+
 #home {
   h1 {
     background: burlywood;
@@ -197,101 +279,41 @@ export default {
     margin: 0;
     padding: 0;
 
-    .red {
-      background: red;
-      height: 100%;
-    }
-    .blue {
-      background: blue;
-      height: 100%;
-    }
-    .green {
-      background: green;
-      height: 100%;
-    }
-    .orange {
-      background: orange;
-      height: 100%;
-    }
-
     #board {
-      #line {
-        width: 20px;
-        height: 20px;
-        background: #000;
-        position: relative;
-        left: 150px;
-      }
+      .one-row {
+        .one-column {
+          flex-direction: row !important;
+          border: 0;
+          color: white;
 
-      .top-1 {
-        top: 35px;
-      }
-      .top-2 {
-        top: 85px;
-      }
-      .top-3 {
-        top: 135px;
-      }
-      .top-4 {
-        top: 185px;
-      }
-      .top-5 {
-        top: 235px;
-      }
-      .top-6 {
-        top: 285px;
-      }
-      .top-7 {
-        top: 335px;
-      }
-      .top-8 {
-        top: 385px;
-      }
-      .top-9 {
-        top: 435px;
-      }
-      .top-10,
-      .top-11 {
-        top: 485px;
-      }
+          .one-cell,
+          .marker {
+            width: 20%;
+            height: 30px;
+            margin: 5px 0;
+          }
 
-      #board-table {
-        margin: auto;
-        width: 40%;
-        background: antiquewhite;
-
-        .pion {
-          width: 10px;
-          height: 10px;
-          display: inline-block;
-        }
-
-        .red {
-           background: red;
-        }
-
-        .black {
-           background: black;
-        }
-
-        .board-col {
-          padding: 0;
-          width: 50px;
-          height: 50px;
-          border: 1px solid #000;
+          .marker {
+            border-left: 1px solid white;
+          }
         }
       }
-    }
 
-    #pattern-table {
-      margin: auto;
-      width: 20%;
+      #pattern {
+        flex-direction: row !important;
+        width: 100%;
+        border-bottom: 0;
 
-      .pattern-col {
-        padding: 0;
-        width: 25px;
-        height: 25px;
-        border: 1px solid #000;
+        .one-cell {
+          margin-top: 10px;
+          width: 20%;
+
+          .pion {
+            &:hover {
+              cursor: pointer;
+            }
+          }
+        }
       }
     }
   }
